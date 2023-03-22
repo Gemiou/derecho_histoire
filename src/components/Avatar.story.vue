9b40222c106427aaeb3bf4588568d83f253c70a7
George Efthymiou <efthymiouge@weatherxm.com>
2023-03-22 11:20:02 +0200

    Init commit

---
+++ src/components/Avatar.story.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import Avatar from './Avatar.vue'
+</script>
+
+<template>
+  <Story title="MyStory">
+    <Variant title="MyVariant 1">
+      <Avatar argument="hello" />
+    </Variant>
+    <Variant title="MyVariant 2">
+      <Avatar argument="world" />
+    </Variant>
+  </Story>
+</template>
---
+++ src/components/Avatar.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import { ref } from 'vue'
+export default {
+  setup() {
+    const count = ref(0)
+    return {
+      count
+    }
+  },
+  mounted() {
+    console.log(this.count) // 0
+  }
+}
+</script>
+
+<template>
+  <button
+    class="bg-transparent light:text-light-primary dark:text-red py-2 px-4 b-rd-[80px] b-2 b-solid font-800 dark:b-dark-primary light:b-light-primary hover:cursor-pointer w-max"
+  >
+    Try again
+  </button>
+</template>
