41989f88f90573de4a2e67ad5a7bba0a21567184
Gemiou <efthymiouge@gmail.com>
2023-03-22 16:37:58 +0200

    Add check boxes

---
+++ src/components/Checkboxes.story.vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import Checkboxes from './Checkboxes.vue'
+</script>
+
+<template>
+  <Story title="Checkboxes" :layout="{ type: 'grid', width: '500px' }">
+    <Variant title="Checkboxes" auto-props-disabled>
+      <Checkboxes :disabled="false" />
+      <Checkboxes :disabled="true" />
+      <Checkboxes :disabled="true" />
+      <Checkboxes :disabled="true" />
+    </Variant>
+    <Variant title="Checkboxes with text" auto-props-disabled>
+      <Checkboxes :disabled="true" :hasText="true">
+        <template v-slot:text> Option Copy </template>
+      </Checkboxes>
+      <Checkboxes :disabled="true" :hasText="true">
+        <template v-slot:text> Option Copy </template>
+      </Checkboxes>
+      <Checkboxes :disabled="true" :hasText="true">
+        <template v-slot:text> Option Copy </template>
+      </Checkboxes>
+      <Checkboxes :disabled="true" :hasText="true">
+        <template v-slot:text> Option Copy </template>
+      </Checkboxes>
+    </Variant>
+  </Story>
+</template>
---
+++ src/components/Checkboxes.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+import { defineProps, ref } from 'vue'
+import CheckMark from './icons/CheckMark.vue'
+defineProps({
+  disabled: {
+    type: Boolean,
+    default: false
+  },
+  hasText: {
+    type: Boolean,
+    default: false
+  }
+})
+let toggle = ref(false)
+</script>
+
+<template>
+  <div class="flex w-full pa-2">
+    <div class="w-full">
+      <label for="checkboxLabelTwo" class="flex cursor-pointer select-none items-center">
+        <div class="relative">
+          <input type="checkbox" id="checkboxLabelTwo" class="sr-only" v-model="toggle" />
+          <div
+            class="box mr-4 flex h-6 w-6 items-center justify-center rounded border bg-[#0A3FAD]"
+            :class="[{ 'bg-[#0A3FAD]': toggle === true }, { 'bg-gray': toggle === false }]"
+          >
+            <CheckMark width="20" height="20" class="text-white"></CheckMark>
+          </div>
+        </div>
+        <span v-if="hasText"> <slot name="text"></slot></span>
+      </label>
+    </div>
+  </div>
+</template>
